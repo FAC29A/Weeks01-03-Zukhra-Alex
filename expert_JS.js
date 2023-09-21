a0471dd2fd257dbde6832e94194890002154a042
Alexander <alex@alexvoiceover.com>
2023-09-21 19:24:23 +0100

    Separeted JS for Expert

--- expert_JS.js
+++ expert_JS.js
@@ -5,13 +5,5 @@ var url = new URL(window.location.href);
 var params = new URLSearchParams(url.search);
 // Retrieve a specific parameter by name
 var paramValue1 = params.get('param1');
-switch (paramValue1) {
-  case '1':
-    segmentSpeed = 0.0002;
-    break;
-  case '2':
-    segmentSpeed = 0.0004;
-    break;
-  default:
-    segmentSpeed = 0.0006;
-}
+
+
