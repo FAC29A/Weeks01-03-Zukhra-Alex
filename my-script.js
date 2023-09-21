e860d28a2e4918bb91b4186a223647029d04d02b
Zukhra <zuhra.galieva.2014@gmail.com>
2023-09-21 19:12:52 +0100

    Refactoring
    Relates #1

--- my-script.js
+++ my-script.js
@@ -37,32 +37,50 @@ const teamMembers = [
     },
 ];
 
-const teamMembersContainer = document.getElementById("team-members");
-
-teamMembers.forEach((member) => {
+// Function to create a card for a team member
+function createTeamMemberCard(member) {
     const card = document.createElement("div");
     card.classList.add("team-member-card");
 
     // Create card content (img, name, position)
     card.innerHTML = `
-        <img src=${member.img} alt="${member.name}" tabindex="${member.id}">
+        <img src="${member.img}" alt="${member.name}" tabindex="${member.id}">
         <h3>${member.name}</h3>
-        <p>${member.position}</p>  
+        <p>${member.position}</p>
     `;
 
     // Add click event listener to the card
-    card.addEventListener("click", navigateToExpertPage);
+    card.addEventListener("click", () => navigateToExpertPage(member.id));
     card.addEventListener("keypress", (event) => {
         if (event.key === "Enter") {
-            navigateToExpertPage();
+            navigateToExpertPage(member.id);
         }
     });
-    
-    function navigateToExpertPage() {
-        // Build the URL with parameters
-        const url = `expert.html?id=${member.id}`;
-        // Navigate to the experts.html page with parameters
-        window.location.href = url;
-    }
-    teamMembersContainer.appendChild(card);
+
+    return card;
+}
+
+// Function to navigate to the expert page
+function navigateToExpertPage(memberId) {
+    // Build the URL with parameters
+    const url = `expert.html?id=${memberId}`;
+    // Navigate to the expert.html page with parameters
+    window.location.href = url;
+}
+
+// Function to create team members cards and append them to the container
+function createTeamMembersCards() {
+    // Get the team members container element
+    const teamMembersContainer = document.getElementById("team-members");
+
+    // Generate cards for each team member and append them to the container
+    teamMembers.forEach((member) => {
+        const card = createTeamMemberCard(member);
+        teamMembersContainer.appendChild(card);
+    });
+}
+
+// Add an event listener for the DOMContentLoaded event
+document.addEventListener('DOMContentLoaded', function () {
+    createTeamMembersCards();
 });
