2d06bba226d097afdece815a1bcc14ecfde53e7e
Alexander <alex@alexvoiceover.com>
2023-09-23 20:59:45 +0100

    Updated names

--- data.js
+++ data.js
@@ -1,50 +1,50 @@
 const teamMembers = [
     {
-        name: "John Doe",
+        name: "Sarah Crane",
         position: "Consultant 1",
         id: 1,
         img: './images/1.jpg',
-        imgBig: "./images/1Big.jpg",
+        imgBig: "./images/01B.png",
         bio: "Chapman was born in Leicester and was raised in Melton Mowbray. He enjoyed science, acting and comedy and, after graduating from Emmanuel College, Cambridge and St Bartholomew's Hospital Medical College, he turned down a career as a doctor to be a comedian. Chapman eventually established a writing partnership with John Cleese, which reached its critical peak with Monty Python during the 1970s. He subsequently left Britain for Los Angeles, where he attempted to be a success on American television, speaking on the college circuit and producing the pirate film Yellowbeard (1983), before returning to Britain in the early 1980s."
     },
     {
-        name: "Jane Smith",
+        name: "Chris Peterson",
         position: "Consultant 2",
         id: 2,
         img: './images/2.jpg',
-        imgBig: "./images/2Big.jpg",
+        imgBig: "./images/02B.png",
         bio: "Chapman was born in Leicester and was raised in Melton Mowbray. He enjoyed science, acting and comedy and, after graduating from Emmanuel College, Cambridge and St Bartholomew's Hospital Medical College, he turned down a career as a doctor to be a comedian. Chapman eventually established a writing partnership with John Cleese, which reached its critical peak with Monty Python during the 1970s. He subsequently left Britain for Los Angeles, where he attempted to be a success on American television, speaking on the college circuit and producing the pirate film Yellowbeard (1983), before returning to Britain in the early 1980s."
     },
     {
-        name: "Mike Johnson",
+        name: "Nicoleta Pagani",
         position: "Consultant 3",
         id: 3,
         img: './images/3.jpg',
-        imgBig: "./images/3Big.jpg",
+        imgBig: "./images/03B.png",
         bio: "Chapman was born in Leicester and was raised in Melton Mowbray. He enjoyed science, acting and comedy and, after graduating from Emmanuel College, Cambridge and St Bartholomew's Hospital Medical College, he turned down a career as a doctor to be a comedian. Chapman eventually established a writing partnership with John Cleese, which reached its critical peak with Monty Python during the 1970s. He subsequently left Britain for Los Angeles, where he attempted to be a success on American television, speaking on the college circuit and producing the pirate film Yellowbeard (1983), before returning to Britain in the early 1980s."
     },
     {
-        name: "Emily Davis",
+        name: "Antonio Salcedo",
         position: "Consultant 4",
         id: 4,
         img: './images/4.jpg',
-        imgBig: "./images/4Big.jpg",
+        imgBig: "./images/04B.png",
         bio: "Chapman was born in Leicester and was raised in Melton Mowbray. He enjoyed science, acting and comedy and, after graduating from Emmanuel College, Cambridge and St Bartholomew's Hospital Medical College, he turned down a career as a doctor to be a comedian. Chapman eventually established a writing partnership with John Cleese, which reached its critical peak with Monty Python during the 1970s. He subsequently left Britain for Los Angeles, where he attempted to be a success on American television, speaking on the college circuit and producing the pirate film Yellowbeard (1983), before returning to Britain in the early 1980s."
     },
     {
-        name: "Chris Wilson",
+        name: "Rajesh Kumar Patel",
         position: "Consultant 5",
         id: 5,
         img: './images/5.jpg',
-        imgBig: "./images/5Big.jpg",
+        imgBig: "./images/05B.png",
         bio: "Chapman was born in Leicester and was raised in Melton Mowbray. He enjoyed science, acting and comedy and, after graduating from Emmanuel College, Cambridge and St Bartholomew's Hospital Medical College, he turned down a career as a doctor to be a comedian. Chapman eventually established a writing partnership with John Cleese, which reached its critical peak with Monty Python during the 1970s. He subsequently left Britain for Los Angeles, where he attempted to be a success on American television, speaking on the college circuit and producing the pirate film Yellowbeard (1983), before returning to Britain in the early 1980s."
     },
     {
-        name: "Lisa Brown",
+        name: "Amina Njoku",
         position: "Consultant 6",
         id: 6,
         img: './images/6.jpg',
-        imgBig: "./images/6Big.jpg",
+        imgBig: "./images/06B.png",
         bio: "Chapman was born in Leicester and was raised in Melton Mowbray. He enjoyed science, acting and comedy and, after graduating from Emmanuel College, Cambridge and St Bartholomew's Hospital Medical College, he turned down a career as a doctor to be a comedian. Chapman eventually established a writing partnership with John Cleese, which reached its critical peak with Monty Python during the 1970s. He subsequently left Britain for Los Angeles, where he attempted to be a success on American television, speaking on the college circuit and producing the pirate film Yellowbeard (1983), before returning to Britain in the early 1980s."
     },
 ];
